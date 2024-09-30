2bf16827719a2b181b6a1fc2729ab92680607cdd
bakhtishod <bakhtishodstudent@gmail.com>
2024-09-30 16:24:55 +0200

    first commit

---
+++ Colony.cs
@@ -0,0 +1,134 @@
+﻿namespace Tundra
+{
+    interface ColonyI
+    {
+        string Name { get; }
+        char Species { get; }
+        int Population { get; }
+        int StartingPopulation { get; }
+
+        void Reproduce(int currentTurn);
+        string ToString();
+    }
+
+    abstract class Colony : ColonyI
+    {
+        public string Name { get; }
+        public char Species { get; }
+        public int Population { get; internal protected set; }
+        public int StartingPopulation { get; }
+
+        protected Colony(string name, char species, int population)
+        {
+            Name = name;
+            Species = species;
+            Population = population;
+            StartingPopulation = population;
+        }
+
+        public abstract void Reproduce(int currentTurn);
+
+        public override string ToString()
+        {
+            return $"{Name} ({Species}): {Population}";
+        }
+    }
+
+    class PreyColony : Colony
+    {
+        public PreyColony(string name, char species, int population) : base(name, species, population) { }
+
+        public override void Reproduce(int currentTurn)
+        {
+            switch (Species)
+            {
+                case 'l':
+                    if (currentTurn % 2 == 0)
+                    {
+                        Population *= 2;
+                    }
+                    break;
+                case 'h':
+                    if (currentTurn % 2 == 0 && Population != 1)
+                    {
+                        Population *= 3 / 2;
+
+                    }
+                    else if (currentTurn % 4 == 0 && Population == 1)
+                    {
+                        Population *= 2;
+                    }
+                    break;
+                case 'g':
+                    if (currentTurn % 4 == 0)
+                    {
+                        Population *= 2;
+
+                    }
+
+                    break;
+            }
+
+        }
+
+    }
+
+    class PredatorColony : Colony
+    {
+
+        public PredatorColony(string name, char species, int population) : base(name, species, population) { }
+
+        public override void Reproduce(int currentTurn)
+        {
+            if (currentTurn % 8 == 0)
+            {
+                Population += GetOffsprings();
+            }
+
+        }
+
+        private int predatorsWithoutPrey = 0;
+
+        public void Attack(Colony preyColony)
+        {
+            if (preyColony != null && preyColony is PreyColony)
+            {
+                int predators = Population;
+                int preyPopulation = preyColony.Population;
+
+                int decreaseTimes = 0;
+                if (preyColony.Species == 'l')
+                {
+                    decreaseTimes = 4;
+                }
+                else
+                {
+                    decreaseTimes = 2;
+                }
+
+                if (preyPopulation >= predators * decreaseTimes)
+                {
+                    preyColony.Population -= predators*decreaseTimes;
+                }
+                else
+                {
+                    preyColony.Population -= (preyPopulation / decreaseTimes) * decreaseTimes;
+                    predatorsWithoutPrey = (predators - (preyPopulation / decreaseTimes));
+                    Population = (preyPopulation / decreaseTimes) + (predatorsWithoutPrey-(predatorsWithoutPrey / decreaseTimes));
+                }
+                
+            }
+        }
+
+        private int GetOffsprings()
+        {
+            switch (Species)
+            {
+                case 'o': return Population / 4;
+                case 'f': return (Population * 3) / 4;
+                case 'w': return (Population / 4)*2;
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using Tundra;
+using TextFile;
+
+
+namespace Tundra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "C:\\Users\\USER\\Desktop\\GitHub\\Tundra\\input.txt";
+
+            TextFileReader reader = new TextFileReader(fileName);
+            int numPrey = int.Parse(reader.ReadLine().Split(' ')[0]);
+
+            int turn = 1;
+            Random random = new Random();
+
+            List<Colony> colonies = ReadColoniesFromFile(fileName, turn);
+
+            Console.WriteLine("Initial input: ");
+            foreach (Colony colony in colonies)
+            {
+                Console.WriteLine(colony);
+            }
+
+            while (true)
+            {
+                Console.WriteLine($"\nTurn {turn}:");
+
+                foreach (Colony colony in colonies)
+                {
+                    colony.Reproduce(turn);
+                }
+
+                foreach (PredatorColony predator in colonies.OfType<PredatorColony>())
+                {
+                    var preyColonies = colonies.OfType<PreyColony>().ToList();
+                    PreyColony targetColony = preyColonies[random.Next(preyColonies.Count)];
+                    predator.Attack(targetColony);
+                }
+
+                int conditionsMet = 0;
+                foreach (Colony colony in colonies)
+                {
+                    if (IsExtinct(colony) || IsQuadrupled(colony))
+                        conditionsMet++;
+                }
+
+                foreach (Colony colony in colonies)
+                {
+                    Console.WriteLine(colony);
+                }
+
+                if (conditionsMet == numPrey)
+                    break;
+
+                turn++;
+
+            }
+        }
+
+        static List<Colony> ReadColoniesFromFile(string fileName, int turnCount)
+        {
+            List<Colony> colonies = new List<Colony>();
+
+            TextFileReader reader = new TextFileReader(fileName);
+
+            string firstLine = reader.ReadLine();
+            int numPrey, numPredators;
+            numPrey = int.Parse(firstLine.Split(' ')[0]);
+            numPredators = int.Parse(firstLine.Split(' ')[1]);
+
+            for (int i = 0; i < numPrey + numPredators; i++)
+            {
+                string line = reader.ReadLine();
+                string[] data = line.Split(' ');
+
+                string name = data[0];
+                char species = char.ToLower(data[1][0]);
+                int population = int.Parse(data[2]);
+
+                if (species == 'l' || species == 'h' || species == 'g')
+                {
+                    colonies.Add(new PreyColony(name, species, population));
+                }
+                else if (species == 'o' || species == 'f' || species == 'w')
+                {
+                    colonies.Add(new PredatorColony(name, species, population));
+                }
+            }
+
+            return colonies;
+        }
+
+        static bool IsExtinct(Colony colony)
+        {
+
+            if (colony is PreyColony preyColony && preyColony.Population <= 0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IsQuadrupled(Colony colony)
+        {
+            if (colony is PreyColony preyColony && preyColony.Population >= preyColony.StartingPopulation * 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
